a0d85bcbad4b106ab4fdf071dd5d7ee838691a9f
fatma.emul <fatma.emul>
2020-02-18 14:11:03 +0300

    stiller eklendi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import GetEmail from './components/GetEmail';
+import mail2 from './mail2.svg';
 
 function App() {
   return (
-    <div className="App">
-      Email Webclient
-        enter email
-        <GetEmail />
-    </div>
+      <div className="App">
+          <header className="App-header">
+              <img src={mail2} className="App-logo" alt="mail1" />
+              Email Upload Page
+          </header>
+          <GetEmail />
+      </div>
   );
 }
+
 
 export default App;
--- src/components/GetEmail.js
+++ src/components/GetEmail.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 import FileUpload from "../core/FileUpload";
 import MsgReader from '@freiraum/msgreader';
 import fs from 'fs';
+import {Panel} from 'primereact/panel';
 
 //var fs = require('fs');
 
@@ -29,6 +30,7 @@ class GetEmail extends Component {
             let testMsgInfo = testMsg.getFileData();
             console.log(testMsgInfo);
 
+            // bunu neden yapmamız gerekti
             let postObject = {...this.state, testMsgInfo};
 
             axios.post("http://localhost:8080/gonderileceklink", postObject)
@@ -45,46 +47,54 @@ class GetEmail extends Component {
 
     };
 
-    render(){
+    render() {
         return <div>
-            <Row>
-                <Col>İsim gizli mi olsun?</Col>
-                <Col>
-                    <div className="isim-radio-button" >
-                        <div className="isim-radio-button" >
-                            <RadioButton inputId="rb1" name="city" value="Evet" onChange={(e) => this.setState({isimGizliMi: e.value})} checked={this.state.isimGizliMi === 'Evet'} />
-                            <label htmlFor="rb1" className="p-radiobutton-label">Evet</label>
+            <Panel className="myPanel" bordered>
+                <Row className="myRow">
+                    <Col xs={6} md={6} className="kelimeler">Would you like to hide your name and surname?</Col>
+                    <Col xs={6} md={4}>
+                        <div className="isim-radio-button">
+                            <div className="isim-radio-button">
+                                <RadioButton inputId="rb1" name="city" value="Evet"
+                                             onChange={(e) => this.setState({isimGizliMi: e.value})}
+                                             checked={this.state.isimGizliMi === 'Evet'}/>
+                                <label htmlFor="rb1" className="p-radiobutton-label">Yes</label>
+                            </div>
+
+                            <div className="isim-radio-button">
+                                <RadioButton inputId="rb2" name="city" value="Hayir"
+                                             onChange={(e) => this.setState({isimGizliMi: e.value})}
+                                             checked={this.state.isimGizliMi === 'Hayir'}/>
+                                <label htmlFor="rb2" className="p-radiobutton-label">No</label>
+                            </div>
                         </div>
+                    </Col>
+                </Row>
+                <Row className="myRow" hidden={this.state.isimGizliMi !== "Hayir"}>
+                    <Col xs={6} md={6} className="kelimeler">Name-Surname</Col>
+                    <Col xs={6} md={4}>
+                        <InputText className="myInput" value={this.state.adSoyad} onChange={(e) => {
+                            this.setState({adSoyad: e.target.value})
+                        }}
+                          />
+                    </Col>
+                </Row>
+                <Row className="myRow">
+                    <Col xs={6} md={6} className="kelimeler">File (Please upload the file extension in eml or msg format)</Col>
+                    <Col>
+                        <FileUpload value={this.state.fileBase64} onChange={(newFileBase64, newFile) => {
+                            this.setState({fileBase64: newFileBase64, file: newFile})
+                        }}
+                                    style={{width: "250px"}}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col><Button onClick={() => {
+                        this.handleButtonClick()
+                    }} variant="primary">Submit</Button></Col>
 
-                        <div className="isim-radio-button" >
-                            <RadioButton inputId="rb2" name="city" value="Hayir" onChange={(e) => this.setState({isimGizliMi: e.value})} checked={this.state.isimGizliMi === 'Hayir'} />
-                            <label htmlFor="rb2" className="p-radiobutton-label">Hayır</label>
-                        </div>
-                    </div>
-                </Col>
-            </Row>
-            <Row hidden={this.state.isimGizliMi !== "Hayir"} >
-                <Col>Ad-Soyad</Col>
-                <Col>
-                    <InputText value={this.state.adSoyad} onChange={(e) => {this.setState({adSoyad: e.target.value})}} />
-                </Col>
-            </Row>
-            <Row >
-                <Col>Dosya</Col>
-                <Col>
-                    <FileUpload value={this.state.fileBase64} onChange={(newFileBase64, newFile) => {this.setState({fileBase64: newFileBase64, file: newFile})}}
-                    style={{width: "450px"}}/>
-                </Col>
-            </Row>
-            <Row >
-                <Col></Col>
-                <Col><Button onClick={() => {this.handleButtonClick()}}
-                             variant="primary" >Gönder</Button></Col>
-
-            </Row>
-
-
-
+                </Row>
+            </Panel>
         </div>;
     }
 }
