909c6cc01f56a06976af5f4f8e048a72693ac281
fatma.emul <fatma.emul>
2020-03-08 16:47:52 +0300

    güncelleme bağlantıları

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
     projectId: "email-ac4fc",
     storageBucket: "email-ac4fc.appspot.com",
     messagingSenderId: "778820633967",
-    appId: "1:778820633967:web:ded0b8e0d6e95f5928949a"
+    appId: "1:778820633967:web:3162a31b3546b42528949a"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
