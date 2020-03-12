54e526c16163512505993b1e6b8e56f665469e3c
fatma.emul <fatma.emul>
2020-03-12 15:01:25 +0300

    yeni veri tabanı ayarı yapıldı

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAni7NU8bOOkCSYUeLYhrnMHl24MXQ2OOQ",
-    authDomain: "email-ac4fc.firebaseapp.com",
-    databaseURL: "https://email-ac4fc.firebaseio.com",
-    projectId: "email-ac4fc",
-    storageBucket: "email-ac4fc.appspot.com",
-    messagingSenderId: "778820633967",
-    appId: "1:778820633967:web:3162a31b3546b42528949a"
+    apiKey: "AIzaSyCSn00N_cgodPJ16Y3h68BDSEOhvk8E-ro",
+    authDomain: "email-5e941.firebaseapp.com",
+    databaseURL: "https://email-5e941.firebaseio.com",
+    projectId: "email-5e941",
+    storageBucket: "email-5e941.appspot.com",
+    messagingSenderId: "114050396172",
+    appId: "1:114050396172:web:7b7cc77cf48f84dc3e7e29"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
